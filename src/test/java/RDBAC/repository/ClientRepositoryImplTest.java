5d8902699ed3160fb49b44c8b46c9bb221b177e1
yurak <200nav1!cvA1cFR>
2019-05-28 12:31:13 +0300

    add tests

--- src/test/java/RDBAC/repository/ClientRepositoryImplTest.java
+++ src/test/java/RDBAC/repository/ClientRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package RDBAC.repository;
 
+import RDBAC.ClientTestData;
 import RDBAC.model.Client;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
@@ -19,21 +22,41 @@ class ClientRepositoryImplTest {
 
     @Test
     void get() {
-        Client client = new Client();
-        client.setId(1);
-        client.setName("Ivanov Viktor");
-        client.setPhone("+375291111111");
         Client clint1FromDb = repository.get(1);
-        assertEquals(client, clint1FromDb);
-    }
-
-    @Test
-    void findByNameAndPhone() {
+        assertEquals(ClientTestData.testClientList.get(0), clint1FromDb);
     }
 
     @Test
     void getAll() {
+        List<Client> clientsListFromDb = repository.getAll();
+        assertIterableEquals(ClientTestData.testClientList, clientsListFromDb);
     }
+
+    @Test
+    void findClientByName() {
+        List<Client> clientsListFromDb = repository.findByNameAndPhone("Koval Alexander");
+        assertEquals(ClientTestData.testClientList.get(2), clientsListFromDb.get(0));
+    }
+
+    @Test
+    void findClientByPhoneNumberWithoutCountryCode() {
+        List<Client> clientsListFromDb = repository.findByNameAndPhone("97777777");
+        assertEquals(ClientTestData.testClientList.get(1), clientsListFromDb.get(0));
+    }
+
+    @Test
+    void findByNameUsingDifferentLetterCase() {
+        List<Client> clientsListFromDb = repository.findByNameAndPhone("KOVal AlEXandEr");
+        assertEquals(ClientTestData.testClientList.get(2), clientsListFromDb.get(0));
+    }
+
+    @Test
+    void findByNameNotExistClient() {
+        List<Client> clientsListFromDb = repository.findByNameAndPhone("Alex Frew");
+        assertEquals(0, clientsListFromDb.size());
+    }
+
+
 
     @Test
     void save() {
