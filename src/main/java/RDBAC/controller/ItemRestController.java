306b695bbaee5f863e70db20a7319219f3d8d8b5
ykazlovich <yura.kozlovich@gmail.com>
2019-08-11 17:47:02 +0300

    Return values in controllers replaced by ResponseEntity<?>

--- src/main/java/RDBAC/controller/ItemRestController.java
+++ src/main/java/RDBAC/controller/ItemRestController.java
@@ -2,11 +2,20 @@ package RDBAC.controller;
 
 import RDBAC.model.Item;
 import RDBAC.service.ItemService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.swing.text.html.parser.Entity;
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = ItemRestController.REST_ITEM_URL)
@@ -19,23 +28,37 @@ public class ItemRestController {
     private ItemService service;
 
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Item> findByModelOrSerialNumber(@RequestParam String search){
+    public List<Item> findByModelOrSerialNumber(@RequestParam String search) {
         return service.findByModelOrSerialNumber(search);
     }
 
     @RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Item getById(@PathVariable int id){
-        return service.get(id);
+    public ResponseEntity<Item> getById(@PathVariable int id) {
+        return Optional
+                .ofNullable(service.get(id))
+                .map(item -> ResponseEntity.ok().body(item))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Item> getAll(){
-        return service.getAll();
+    public ResponseEntity<List<Item>> getAll() {
+        List<Item> entities = service.getAll();
+        return Optional.of(entities)
+                .map(items -> ResponseEntity.status(HttpStatus.OK).body(items))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping(path = "/")
-    public Item save(@RequestBody Item item){
-        return service.save(item);
+    public ResponseEntity<Item> save(@RequestBody Item item) {
+        Item save = service.save(item);
+        return Optional.ofNullable(save)
+                .map(i -> ResponseEntity.created(ServletUriComponentsBuilder
+                        .fromCurrentContextPath()
+                        .path(REST_ITEM_URL + "/{id}")
+                        .buildAndExpand(i.getId())
+                        .toUri())
+                        .body(i))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 
 
--- src/main/java/RDBAC/controller/OrderRestControllerImpl.java
+++ src/main/java/RDBAC/controller/OrderRestControllerImpl.java
@@ -7,8 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = OrderRestControllerImpl.REST_ORDER_URL)
@@ -21,20 +25,31 @@ public class OrderRestControllerImpl {
     private OrderService service;
 
     @RequestMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Order> getAllOrder() {
-        return service.getAll();
+    public ResponseEntity<List<Order>> getAllOrder() {
+        List<Order> orders = service.getAll();
+        return Optional.ofNullable(orders)
+                .map(o -> ResponseEntity.status(HttpStatus.OK).body(o))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @RequestMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Order getOrderById(@PathVariable(name = "id") int id) {
-        return service.get(id);
+    public ResponseEntity<Order> getOrderById(@PathVariable(name = "id") int id) {
+        Order order = service.get(id);
+        return Optional.ofNullable(order)
+                .map(o -> ResponseEntity.status(HttpStatus.OK).body(o))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
     }
 
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Order> save(@RequestBody Order order){
+    public ResponseEntity<Order> save(@RequestBody Order order) {
         Order addedOrder = service.save(order);
-        ResponseEntity<Order> o = new ResponseEntity<>(addedOrder, HttpStatus.CREATED);
-        return o;
+        return Optional.ofNullable(addedOrder)
+                .map(o -> ResponseEntity.created(ServletUriComponentsBuilder
+                        .fromCurrentContextPath()
+                        .path(REST_ORDER_URL + "/{id}").build().toUri()).body(o))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
     }
 
 
